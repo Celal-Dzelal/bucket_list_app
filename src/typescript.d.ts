d270078f0919671feeec146be2c992cb00494a6e
Celal <binaycelalselim@gmail.com>
2025-02-26 01:00:34 +0300

    project reset

--- src/typescript.d.ts
+++
@@ -1,9 +0,0 @@
-interface ITodo {
-  task: string;
-  isDone: boolean;
-  id: string;
-}
-
-type AddFn = (task: string) => Promise<void>;
-type ToggleFn = (todo: ITodo) => Promise<void>;
-type DeleteFn = (id: string | number) => Promise<void>;
