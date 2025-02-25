7fd755f6b765ef90ef472d481913b5e8dc558380
Celal <binaycelalselim@gmail.com>
2025-02-26 01:37:45 +0300

    Add Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,24 @@
+import { Typography } from "@mui/material";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Typography
+      component="h1"
+      variant="h2"
+      align="center"
+      sx={{
+        background: "linear-gradient(90deg,#769DA6,#254559)",
+        WebkitBackgroundClip: "text",
+        WebkitTextFillColor: "transparent",
+        fontWeight: "bold",
+        textShadow: "3px 3px 5px rgba(0,0,0,0.3)",
+        padding: "1rem",
+        marginBottom: "1.5rem",
+      }}
+    >
+      Bucket List
+    </Typography>
+  );
 };
 
 export default Header;
