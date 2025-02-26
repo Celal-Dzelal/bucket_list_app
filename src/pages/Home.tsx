07e92b29fc0c8829602329b186a535b4bff7a7e5
Celal <binaycelalselim@gmail.com>
2025-02-26 16:14:21 +0300

    api axios

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,35 @@
+import axios from "axios";
 import AddTodo from "../components/AddTodo";
 import Header from "../components/Header";
 import TodoList from "../components/TodoList";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  interface ITodo {
+    id: string;
+    isDone: boolean;
+    task: string;
+  }
+
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const getTodo = async () => {
+    const { data } = await axios(apiUrl);
+    console.log(data);
+    setTodos(data);
+  };
+
+  useEffect(() => {
+    getTodo();
+  }, []);
+
   return (
     <div>
       <Header />
       <AddTodo />
-      <TodoList />
+      <TodoList todos={todos} />
     </div>
   );
 };
